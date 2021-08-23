035f3b340b75fc7add2f097878e5154135c8c639
Gametz <ls.vlad@bk.ru>
2021-08-23 22:33:35 +0300

    1.0

--- flask_app.py
+++ flask_app.py
@@ -4,8 +4,19 @@ from settings import *
 import messageHandler
 import vk
 import random
+import git
 
 app = Flask(__name__)
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/')
 def hello_world():
